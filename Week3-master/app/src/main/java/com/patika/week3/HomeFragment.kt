56f6801781d1ed7fe304ae2dc37c53bd670157ef
Ahmet Furkan Sevim <ahmetfurkansevim5.gmail.com>
2022-09-11 00:36:35 +0300

    add logic and ui

--- Week3-master/app/src/main/java/com/patika/week3/HomeFragment.kt
+++ Week3-master/app/src/main/java/com/patika/week3/HomeFragment.kt
@@ -1,88 +1,91 @@
 package com.patika.week3
 
-import android.content.Context
+import android.content.res.Resources
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.patika.week3.databinding.FragmentHomeBinding
+import kotlin.math.roundToInt
 
 
 class HomeFragment : Fragment() {
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        Log.v("PATIKADEV","onAttach called.")
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.v("PATIKADEV","onCreate called.")
-    }
+    private lateinit var binding: FragmentHomeBinding
+    var targetWater = 0.0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.v("PATIKADEV","onCreateView called.")
-        return inflater.inflate(R.layout.fragment_home,
-            container,
-            false
-        )
-
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.v("PATIKADEV","onViewCreated called.")
-    }
-
-    override fun onViewStateRestored(savedInstanceState: Bundle?) {
-        super.onViewStateRestored(savedInstanceState)
-        Log.v("PATIKADEV","onViewStateRestored called.")
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.v("PATIKADEV","onStart called.")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.v("PATIKADEV","onResume called.")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.v("PATIKADEV","onPause called.")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.v("PATIKADEV","onStop called.")
+        binding.apply {
+            smallBottle.setOnClickListener{
+                drinkWater(0.2)
+            }
+            mediumBottle.setOnClickListener{
+                drinkWater(0.5)
+            }
+            bigBottle.setOnClickListener{
+                drinkWater(1.0)
+            }
+            clearAll.setOnClickListener{
+                targetWater = 0.0
+                drinkWater(0.0)
+            }
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        Log.v("PATIKADEV","onSaveInstanceState called.")
+        outState.putDouble("targetWater", targetWater)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        Log.v("PATIKADEV","onDestroyView called.")
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        targetWater = savedInstanceState?.getDouble("targetWater", 0.0)?:0.0
+        drinkWater(0.0)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.v("PATIKADEV","onDestroy called.")
+    fun backgroundColor(){
+        var screenHeight = Resources.getSystem().displayMetrics.heightPixels
+        var ratio =  targetWater / 3.0
+        var height = (screenHeight * ratio).toInt()
+
+        var params = binding.background.layoutParams
+        params.height = height
+        binding.background.layoutParams = params
+  }
+
+    fun drinkWater(bottle:Double){
+        targetWater += bottle
+        backgroundColor()
+        textChange()
     }
 
-    override fun onDetach() {
-        super.onDetach()
-        Log.v("PATIKADEV","onDetach called.")
+    fun textChange(){
+        binding.apply {
+            val water = ((3.0 - targetWater) * 100.0).roundToInt() / 100.0
+            when (targetWater){
+                0.0 -> {
+                    header1.text = "Merhaba! \nBugün su içtin mi?"
+                    header2.text = "Bugünkü su ihtiyacını karşılamak için \n3 litre daha su içmelisin."
+                }
+                in 0.2..2.9 -> {
+                    header1.text = "Tebrikler! \nDoğru yoldasın."
+                    header2.text = "Bugünkü su ihtiyacını karşılamak için \n$water litre daha su içmelisin."
+                }
+                in 2.9..5.0 -> {
+                    header1.text = "Muhteşem! \nKendine iyi baktın."
+                    header2.text = "Bugün vücüdun için gereken su miktarının tamamını karşıladın."
+                }
+            }
+        }
     }
-
-
 
 }
